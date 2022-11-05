e7335b2c11673351514c9628678b46a23af6fad3
Shreya Ghosh <sgshreya024@gmail.com>
2022-11-05 11:57:00 +0530

    Drag & Drop Implemented

--- diysaree/src/components/DIYDesignEditor/DIYDesignEditor.jsx
+++ diysaree/src/components/DIYDesignEditor/DIYDesignEditor.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import { useRef, useState, React } from "react";
 import DIYCSS from "./DIYDesignEditor.module.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { Stage, Image, Layer } from "react-konva";
+import useImage from "use-image";
+
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
+  return (
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      // I will use offset to set origin to the center of the image
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+    />
+  );
+};
 
 const DIYDesignEditor = () => {
+  const dragUrl = useRef();
+  const stageRef = useRef();
+  const [images, setImages] = useState([]);
   return (
     <div className={DIYCSS.outer}>
+      Try to trag and image into the stage:
+      <br />
+      <img
+        alt="lion"
+        src="https://konvajs.org/assets/lion.png"
+        draggable="true"
+        onDragStart={(e) => {
+          dragUrl.current = e.target.src;
+        }}
+      />
       <Row>
         <Col>a</Col>
         <Col>b</Col>
@@ -15,13 +44,41 @@ const DIYDesignEditor = () => {
         <Col>d</Col>
         <Button variant="dark">Save Design</Button>
       </Row>
-
       <Row>
         <Button variant="dark">Add to Wishlist</Button>
         <Button variant="dark">Order Now</Button>
       </Row>
-
-      <Container className={DIYCSS.inner}></Container>
+      <Container
+        className={DIYCSS.inner}
+        onDrop={(e) => {
+          e.preventDefault();
+          // register event position
+          stageRef.current.setPointersPositions(e);
+          // add image
+          setImages(
+            images.concat([
+              {
+                ...stageRef.current.getPointerPosition(),
+                src: dragUrl.current,
+              },
+            ])
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={{ border: "1px solid grey" }}
+          ref={stageRef}
+        >
+          <Layer>
+            {images.map((image) => {
+              return <URLImage image={image} />;
+            })}
+          </Layer>
+        </Stage>
+      </Container>
     </div>
   );
 };
