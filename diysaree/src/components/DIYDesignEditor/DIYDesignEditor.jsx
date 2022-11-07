1420e6ae87c558c1b79d827b765f5778e0015913
Shreya Ghosh <sgshreya024@gmail.com>
2022-11-07 20:35:04 +0530

    Implemented Color Picker functionality

--- diysaree/src/components/DIYDesignEditor/DIYDesignEditor.jsx
+++ diysaree/src/components/DIYDesignEditor/DIYDesignEditor.jsx
@@ -8,7 +8,9 @@ import { Stage, Image, Layer } from "react-konva";
 import useImage from "use-image";
 import block1 from "./block1.png";
 import block2 from "./block2.png";
-import SketchExample from "./Sketch";
+import { ReactComponent as MyLogo } from "./icon2.svg";
+import reactCSS from "reactcss";
+import { SketchPicker } from "react-color";
 
 const URLImage = ({ image }) => {
   const [img] = useImage(image.src);
@@ -28,6 +30,56 @@ const DIYDesignEditor = () => {
   const dragUrl = useRef();
   const stageRef = useRef();
   const [images, setImages] = useState([]);
+
+  const [colorPickerstate, setColorPickerState] = useState(false);
+  const [color, setColor] = useState({ r: "241", g: "112", b: "19", a: "1" });
+
+  const handleClick = () => {
+    setColorPickerState(!colorPickerstate);
+  };
+
+  const handleClose = () => {
+    setColorPickerState(false);
+  };
+
+  const handleChange = (e) => {
+    setColor({
+      r: e.rgb.r,
+      g: e.rgb.g,
+      b: e.rgb.b,
+      a: e.rgb.a,
+    });
+  };
+
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: "36px",
+        height: "14px",
+        borderRadius: "2px",
+        background: `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
+      },
+      swatch: {
+        padding: "5px",
+        background: "#fff",
+        borderRadius: "1px",
+        boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
+        display: "inline-block",
+        cursor: "pointer",
+      },
+      popover: {
+        position: "absolute",
+        zIndex: "2",
+      },
+      cover: {
+        position: "fixed",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
+      },
+    },
+  });
   return (
     <Container className={DIYCSS.outer}>
       <Row>
@@ -51,8 +103,19 @@ const DIYDesignEditor = () => {
             }}
           />{" "}
         </Col>
+
         <Col>
-          <SketchExample />
+          <div>
+            <div style={styles.swatch} onClick={handleClick}>
+              <div style={styles.color} />
+            </div>
+            {colorPickerstate ? (
+              <div style={styles.popover}>
+                <div style={styles.cover} onClick={handleClose} />
+                <SketchPicker color={color} onChange={handleChange} />
+              </div>
+            ) : null}
+          </div>
         </Col>
         <Col>
           <Button variant="dark">Save Design</Button>
